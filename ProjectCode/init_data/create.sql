58a1a1e0aaaa9ade9d8ab404547a536407705dea
Lillian Duarte <lidu5648@colorado.edu>
2022-11-08 16:58:18 -0700

    addded another database table and added insert data for the database to allow testing. also created login and add friends API route

--- ProjectCode/init_data/create.sql
+++ ProjectCode/init_data/create.sql
@@ -2,3 +2,8 @@ CREATE TABLE users(
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
+CREATE TABLE friends(
+    username VARCHAR(50),
+    friend_username VARCHAR(50)
+);
