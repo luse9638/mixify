68e0f21b7c2b9a3b898df326efe3ff28c0f82f80
Lillian Duarte <lidu5648@colorado.edu>
2022-11-11 13:46:55 -0700

    updated this to be where main is and adding the app post request that needs to be edited to work with the spotify API

--- ProjectCode/index.js
+++ ProjectCode/index.js
@@ -95,6 +95,80 @@ app.get('/loginSpotify', function(req, res) {
     }));
 });
 
+
+
+// CHANGES: created a post login to submit information like username
+// will probably need to replace with the spotify API but for now
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const query = "select * from users where users.username = $1";
+
+  db.one(query, values)
+    .then((data) => {
+      user.username = username;
+      req.session.user = user;
+      req.session.save();
+
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/login");
+    });
+});
+
+
+app.get("/login", function (req, res) {
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = "user-read-private user-read-email user-top-read";
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
+  );
+});
+
+
+
+
+// not tested yet but this should add to the friends table with the friend requested 
+const all_friends = `
+  SELECT friends.friend_username
+  WHERE friends.username = $1
+  FROM friends;
+  `;
+
+app.post("/prospects/add", (req, res) => {
+  const username = req.body.username;
+  db.tx(async (t) => {
+    await t.none(
+      "INSERT INTO friends(username, friend_username) VALUES ($1, $2);",
+      [username, req.session.user.friend_username] 
+      // not sure how to connect friend_id to ejs page
+    );
+    return t.any(all_friends, [req.session.user.username]);
+  })
+    .then(() => {
+      res.render("pages/prospects", {
+        message: `Successfully added friend ${req.body.friend_username}`,
+      });
+    })
+    .catch((err) => {
+      res.render("pages/prospects", {
+        error: true,
+        message: err.message,
+      });
+    });
+});
+
   
 app.listen(3000, () => {
   console.log('listening on port 3000');
