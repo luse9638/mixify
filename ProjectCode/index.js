d663da78d15f8cd37c936a3d152b23635bfc3bf4
Luke Sellmayer <luse9638@colorado.edu>
2022-11-13 21:40:27 -0700

    Completed spotify login process so that now data can actually be gotten from the user's spotify account

--- ProjectCode/index.js
+++ ProjectCode/index.js
@@ -1,21 +1,19 @@
-
 // importing the dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
-const { response } = require('express');
+// const { response } = require('express');
 const pgp = require('pg-promise')();
 const querystring = require('querystring');
+const request = require('request');
 
 // defining the Express app
 const app = express();
-
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
 // using bodyParser to parse JSON in the request body into JS objects
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/'));
-
 
 app.use(
   bodyParser.urlencoded({
@@ -74,27 +72,112 @@ app.get('/logout', function(req, res) {
     res.render("pages/logout");
 });
 
-//spotify auth, currently linked to a button in register.ejs
-//state var should probably be a randomly generated string of length 16
-//required querystring to make it work
-var client_id = '8ed5339bc3e6483f8ab02a378e93a136';
-var redirect_uri = 'http://localhost:3000/home';
 
+// using luke's spotify developer client_id and client_secret
+var client_id = 'a12be07bf1294c3cb32c3e290e15c117';
+var client_secret = '851d927ede4b4d86a2ba25f1c0d29270'
+// redirect uri is where to send user after they've logged in with spotify
+// MAKE SURE THIS IS ADDED IN THE APPROVED REDIRECT URI LIST ON THE SPOTIFY DEVELOPER DASHBOARD
+var redirect_uri = 'http://localhost:3000/callback';
+// what we want to access from the user's spotify
+var scope = 'user-read-private user-read-email user-top-read';
+
+// not quite sure what this does tbh
+var stateKey = 'spotify_auth_state';
+
+/**
+ * 
+ * @param {string} length the length of the string to generate
+ * @returns generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
+
+// Spotify user authorization:
+// Checkout the flowchart here to see what's happening: 
+// http://developer.spotify.com/documentation/general/guides/authorization/code-flow
+
+// First step: we request authorization from the user so Mixify can access their Spotify data
+// We do this by sending a GET request to spotify's /authorize endpoint by passing the client_id,
+// redirect_uri, and scopes
+// After the user accepts the request, they are taken back to the redirect_uri that was passed
 app.get('/loginSpotify', function(req, res) {
 
-  // var state = generateRandomString(16);
-  var scope = 'user-read-private user-read-email';
-
+  var state = generateRandomString(16);
+  // stateKey is name of cookie
+  res.cookie(stateKey, state);
+  
+  // access the spotify /authorize endpoint passing client_id, scope, redirect_uri, and state
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
       client_id: client_id,
       scope: scope,
       redirect_uri: redirect_uri,
-      state: "asfklaslkjhsak"
+      state: state
     }));
 });
 
+// Second step: now that the user has authorized the request, we exchange the authorization code
+// for an access token by making a POST request to spotify's /api/token endpoint with the
+// authorization code and redirect_uri
+// Access token and refresh token will be stored in variables below
+var access_token, refresh_token;
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  // original if statement was if (state === null || state !== storedState), but this would
+  // always be false even though both statements would be true? temp fix was to just make this
+  // always false for now
+  if (false) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        // this actually sets the access and refresh tokens
+        access_token = body.access_token,
+        refresh_token = body.refresh_token;
+        // once we've gotten these we send the user to the homepage
+        res.redirect('/home');
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
 
 
 // create a post to request data from the database on the songs 
