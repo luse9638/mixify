695951a9ea8952d550d24a612b49d73aca8ece0b
Lillian Duarte <lidu5648@colorado.edu>
2022-11-18 13:35:13 -0700

    updated insert.sql to reflect changes in tables

--- ProjectCode/init_data/insert.sql
+++ ProjectCode/init_data/insert.sql
@@ -4,12 +4,16 @@ INSERT INTO users(userID, displayName, profilePicURL) VALUES
 ('93hgk2hfiy29thf83hf93jd', 'Justin','https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png');
 
 
-
 INSERT INTO friends(userID, friendUserID) VALUES
 ('lillian', 'luke'),
 ('luke', 'lillian');
 
+INSERT INTO lillian_songs(song, artist) VALUES
+('Butterfly Effect', 'Travvy Scott'),
+('Yosemite', 'Travvy Scott'),
+('You Only Live Once', 'The Strokes');
 
-INSERT INTO allUsersAllSongs(userID, songName) VALUES
-('lillian', 'reptilia'),
-('luke', 'reptilia');
+INSERT INTO justin_songs(song, artist) VALUES
+('Instant Crush', 'Daft Punk'),
+('Butterfly Effect', 'Travvy Scott'),
+('Equinox', 'Hotel Pools');
